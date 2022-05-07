f729f903b4b57af6bb3265e781abac0b920f1660
bibhuprasadrout <bibhu.rs@gmail.com>
2022-05-07 23:35:56 +0530

    [feat]Add css components to the library
    changes were made to the file structure, many old
    file were deleted.
    all components and utilities(utilityComponents) were
    placed inside the components file.
    the css class names were imporved.
    colors were added along with other components.

--- components/navBar/navBar.js
+++ components/navBar/navBar.js
@@ -1,8 +1,5 @@
 // mobile menu onclick event
-const mobileNavMenu = document.querySelector('.navMenuMobile')
-const mobileNavMenuicon = document.querySelector('.navMenuMobileIcon')
+const mobileNavMenuicon = document.querySelector('#navMenuMobileIcon')
 mobileNavMenuicon.addEventListener('click', () => {
-  console.log('abc') 
-mobileNavMenu.classList.remove('hidden')
-mobileNavMenu.classList.add('visible')
+  mobileNavMenuicon.nextElementSibling.classList.toggle('hidden')
 })
